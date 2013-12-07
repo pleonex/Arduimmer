e9c3241873b415ae0ea694ef4e1494bde6ef44eb
Benito Palacios <benito356@gmail.com>
2013-12-07 15:10:38 +0100

    Implemented Receive and Send methods

--- Sketchs/picProgrammer.ino
+++ Sketchs/picProgrammer.ino
@@ -3,6 +3,13 @@ const int pinPGC = 6;
 const int pinPGD = 7;
 const int pinVPP = 2;
 
+void picProgrammerSetup() {
+  pinMode(pinPGM, OUTPUT);
+  pinMode(pinPGC, OUTPUT);
+  pinMode(pinPGD, OUTPUT);
+  pinMode(pinVPP, OUTPUT); 
+}
+
 void enterLowVoltageIcsp() {
   // 1º Set PGC & PGD low
   digitalWrite(pinPGC, LOW);
@@ -31,4 +38,38 @@ void exitLowVoltageIcsp() {
   
   // 3º Set PGM low
   digitalWrite(pinPGM, LOW);
+}
+
+void sendBit(bit data) {
+  digitalWrite(pinPGC, HIGH);
+  digitalWrite(pinPGD, data);
+  
+  digitalWrite(pinPGC, LOW);
+  digitalWrite(pinPGD, LOW);
+}
+
+void sendBits(short data, int n) {
+  for (int i = 0; i < n; i++)
+    sendBit(bitRead(data, i));
+}
+
+byte receiveByte() {
+  // Set PGD low and wait 8 clock cycles
+  digitalWrite(pinPGD, LOW);
+  for (byte i = 0; i < 8; i++) {
+    digitalWrite(pinPGC, HIGH);
+    digitalWrite(pinPGC, LOW);
+  }
+  
+  // Read byte from PGD
+  pinMode(pinPGD, INPUT);
+  byte data = 0;
+  for (byte i = 0; i < 8; i++) {
+    digitalWrite(pinPGC, HIGH);
+    bitWrite(data, i, digitalRead(pinPGC));
+    digitalWrite(pinPGC, LOW);
+  }
+  
+  // Set again PGD as output
+  pinMode(pinPGD, OUTPUT);
 }
