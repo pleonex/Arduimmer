2950f714d8e6dbd670061fdffec10e60f396b238
Benito Palacios <benito356@gmail.com>
2013-12-09 02:28:21 +0100

    Added licence file and headers

--- Sketchs/picProgrammer.ino
+++ Sketchs/picProgrammer.ino
@@ -1,3 +1,22 @@
+/*
+    Arduimmer. Using Arduino as a PIC18F2XXX and PIC18F4XXX programmer
+    Copyright (C) 2013 Benito Palacios (benito356@gmail.com)
+  
+    This file is part of Arduimmer.
+
+    Arduimmer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Arduimmer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Arduimmer.  If not, see <http://www.gnu.org/licenses/>. 
+*/
 void picProgrammerSetup() {
   pinMode(pinPGM, OUTPUT);
   pinMode(pinPGC, OUTPUT);
