0ceb5cd48b5f8784874cc4b3405e8d6bc5858883
Benito Palacios Sánchez <benito356@gmail.com>
2015-03-28 11:38:58 +0100

    Convert defines to constant to optimize program (RAM issues)

--- Sketchs/src/TIbeeProgrammer.h
+++ Sketchs/src/TIbeeProgrammer.h
@@ -21,37 +21,37 @@
 #include "Arduino.h"
 #include "IcspProgrammer.h"
 
-#define CMD_DEBUG_INSTR_1  B01010001    // Run 1 instruction in CPU
-#define CMD_DEBUG_INSTR_2  B01010010    // Run 2 instructions in CPU
-#define CMD_DEBUG_INSTR_3  B01010011    // Run 3 instructions in CPU
-#define CMD_CHIP_ERASE     B00010000    // Erase the chip
-#define CMD_WR_CONFIG      B00011001    // Write debug configuration
-#define CMD_RD_CONFIG      B00100000    // Read debug configuration
-#define CMD_BURST_WRITE    B10000000    // Write data to the buffer
+const byte CMD_DEBUG_INSTR_1 = B01010001;    // Run 1 instruction in CPU
+const byte CMD_DEBUG_INSTR_2 = B01010010;    // Run 2 instructions in CPU
+const byte CMD_DEBUG_INSTR_3 = B01010011;    // Run 3 instructions in CPU
+const byte CMD_CHIP_ERASE    = B00010000;    // Erase the chip
+const byte CMD_WR_CONFIG     = B00011001;    // Write debug configuration
+const byte CMD_RD_CONFIG     = B00100000;    // Read debug configuration
+const byte CMD_BURST_WRITE   = B10000000;    // Write data to the buffer
 
 
 // DUP registers (XDATA space address)
-#define DUP_DBGDATA   0x6260  // Debug interface data buffer
-#define DUP_FCTL      0x6270  // Flash controller
-#define DUP_FWDATA    0x6273  // Flash controller data buffer
-#define DUP_FADDRL    0x6271  // Flash controller addr
-#define DUP_FADDRH    0x6272  // Flash controller addr
-#define DUP_DMA1CFGL  0x70D2  // Low byte, DMA config ch. 1
-#define DUP_DMA1CFGH  0x70D3  // High byte, DMA config ch. 1
-#define DUP_DMA0CFGL  0x70D4  // Low byte, DMA config ch. 0
-#define DUP_DMA0CFGH  0x70D5  // High byte, DMA config ch. 0
-#define DUP_DMAARM    0x70D6  // DMA arming register
-#define DUP_MEMCTR    0x70C7  // Flash bank xdata mapping
-#define DUP_CLKCONSTA 0x709E  // Sys clock status
-#define DUP_CLKCONCMD 0x70C6  // Sys clock configuration
+const unsigned short DUP_DBGDATA   = 0x6260;  // Debug interface data buffer
+const unsigned short DUP_FCTL      = 0x6270;  // Flash controller
+const unsigned short DUP_FWDATA    = 0x6273;  // Flash controller data buffer
+const unsigned short DUP_FADDRL    = 0x6271;  // Flash controller addr
+const unsigned short DUP_FADDRH    = 0x6272;  // Flash controller addr
+const unsigned short DUP_DMA1CFGL  = 0x70D2;  // Low byte, DMA config ch. 1
+const unsigned short DUP_DMA1CFGH  = 0x70D3;  // High byte, DMA config ch. 1
+const unsigned short DUP_DMA0CFGL  = 0x70D4;  // Low byte, DMA config ch. 0
+const unsigned short DUP_DMA0CFGH  = 0x70D5;  // High byte, DMA config ch. 0
+const unsigned short DUP_DMAARM    = 0x70D6;  // DMA arming register
+const unsigned short DUP_MEMCTR    = 0x70C7;  // Flash bank xdata mapping
+const unsigned short DUP_CLKCONSTA = 0x709E;  // Sys clock status
+const unsigned short DUP_CLKCONCMD = 0x70C6;  // Sys clock configuration
 
 // DMA channels used on DUP
-#define CH_DBG_TO_BUF0   0x01 // Channel 0
-#define CH_BUF0_TO_FLASH 0x02 // Channel 1
+const byte CH_DBG_TO_BUF0   = 0x01; // Channel 0
+const byte CH_BUF0_TO_FLASH = 0x02; // Channel 1
 
 // Start addresses on DUP (Increased buffer size improves performance)
-#define ADDR_BUF0       0x0000 // Buffer (512 bytes)
-#define ADDR_DMA_DESC_0 0x0200 // DMA descriptors (8 bytes)
+const unsigned short ADDR_BUF0       = 0x0000; // Buffer (512 bytes)
+const unsigned short ADDR_DMA_DESC_0 = 0x0200; // DMA descriptors (8 bytes)
 #define ADDR_DMA_DESC_1 (ADDR_DMA_DESC_0 + 8)
 
 class TIbeeProgrammer : public IcspProgrammer
